999f6b1d28159648c9ccf4a8620610ea1209ac05
guruprasadss0512 <guruprasadss0512@gmail.com>
2026-04-21 10:12:20 +0000

    credentials removed

--- zoho_auth.py
+++ zoho_auth.py
@@ -1,11 +1,5 @@
 import requests
 
-# Fill these in!
-CLIENT_ID = "1000.19SS97YUP3Q6TVB2M8ROD6VTOGMG9F"
-CLIENT_SECRET = "4e077a0f2075d85bcd5973aaa933342ed85d332db0"
-GRANT_TOKEN = "1000.e48dbc7534ff32a0a7b0794383dfdcaa.2405c6b0fa4840f8741e1bc8b8111d66"
-REDIRECT_URI = "http://localhost:8000/callback"
-
 # Change .in to .com if your account is not on the Indian data center
 url = "https://accounts.zoho.in/oauth/v2/token"
 
